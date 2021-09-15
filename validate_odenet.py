e11c835e8ae9885fbc5db8106329bb71df0af176
Melanie Siegel <melanie.siegel@h-da.de>
2021-09-15 12:03:24 +0200

    ILI Corrections by Johann Bergh
    No duplicate ILIs any more!

--- validate_odenet.py
+++ validate_odenet.py
@@ -3,42 +3,96 @@
 
 # Importe
 
-from odenet import *
+#from odenet import *
+from xml.etree import ElementTree as ET
 from lxml import etree
+import re
+
+g_wordDict = None
+
+########## Zugriff auf das Wordnet #######
+
+def get_wordnet_lexicon_local(wnfile):
+     loc_wn = open(wnfile,"r",encoding="utf-8")
+     wntree = ET.parse(loc_wn)
+     wnroot = wntree.getroot()
+     lexicon = wnroot.find('Lexicon')
+     return lexicon
+
 
 ########## LexEntries ######
+def get_word_dict(wordnet):
+    lexicon = get_wordnet_lexicon_local(wordnet)            
+    wordDict = {}        
+    for synset in lexicon.findall('./Synset'):
+        wordDict[synset.attrib['id']] = []
+    for lexentry in lexicon.findall('./LexicalEntry'):
+        for sense in lexentry.findall('./Sense'):                
+            lemma = lexentry.find('Lemma').attrib['writtenForm']
+            wordDict[sense.attrib['synset']].append(lemma)
+
+    return wordDict        
+
+
+def check_word_id(word_id, wordnet):
+    lexicon = get_wordnet_lexicon_local(wordnet)
+    for lexentry in lexicon.iter('LexicalEntry'):
+        lemma = lexentry.find('Lemma')
+        lemma_value = lemma.attrib['writtenForm']
+        lemma_id = lexentry.attrib['id']
+        if lemma_id == word_id:
+            pos = lemma.attrib['partOfSpeech']
+            senses = []
+            for sense in lexentry.iter('Sense'):
+                sense_id = sense.attrib['id']
+                synset_id = sense.attrib['synset']
+                senses.append([sense_id,synset_id])
+            print("LEMMA: " + lemma_value + "\nPOS: " + pos + "\nSENSE ID: " + sense_id)
+
+def find_all_lexentries(word_to_check, wordnet):
+    lexicon = get_wordnet_lexicon_local(wordnet)
+    for lexentry in lexicon.iter('LexicalEntry'):
+        lemma = lexentry.find('Lemma')
+        lemma_value = lemma.attrib['writtenForm']
+        lemma_id = lexentry.attrib['id']
+        word_id_list = []
+        if lemma_value == word_to_check:
+            word_id_list.append(lemma_id)
+        for wid in word_id_list:
+            check_word_id(wid, wordnet)    
 
 # Prüfung, ob es für ein Lemma mehrere LexEntries gibt
-# Das sind immer noch ganz schön viele!
-# Daher nicht in Standard-Test aufgenommen.
 
-def find_duplicate_lexentries():
+def find_duplicate_lexentries(wordnet):
+    lexicon = get_wordnet_lexicon_local(wordnet)
     lemma_list = []
     for lexentry in lexicon.iter('LexicalEntry'):
         lemma = lexentry.find('Lemma')
         lemma_value = lemma.attrib['writtenForm']
         lemma_id = lexentry.attrib['id']
         if lemma_value in lemma_list:
-            find_all_lexentries(lemma_value)
+            find_all_lexentries(lemma_value,wordnet)
         else:
             lemma_list.append(lemma_value)
-    print("Es gibt doppelte Einträge für " + str(len(lemma_list)) + " Lemmata!")
+#    print("Es gibt doppelte Einträge für " + str(len(lemma_list)) + " Lemmata!")
 
 # Prüfung, ob die Lex-ID mit der Sense-ID übereinstimmt
 
-def find_inconsistent_lexids():
+def find_inconsistent_lexids(wordnet):
+    lexicon = get_wordnet_lexicon_local(wordnet)
     for lexentry in lexicon.iter('LexicalEntry'):
         lemma_id = lexentry.attrib['id']
         for sense in lexentry.iter('Sense'):
             sense_id = sense.attrib['id']
             sense_w_id = sense_id.split('_')[0]
             if sense_w_id != lemma_id:
-                print ("LexIDs inkonsistent in: " + lemma_id)
+                print ("LexIDs inkonsistent in: " + lemma_id + ": " +  sense_w_id)
 
 # Prüfung, ob eine Synset-ID im LexEntry überhaupt existiert
 # und ob eine Synset-ID in einer Relation überhaupt existiert
 
-def test_for_existance_of_synset():
+def test_for_existance_of_synset(wordnet):
+    lexicon = get_wordnet_lexicon_local(wordnet)
     list_of_synsets = []
     for synset in lexicon.iter('Synset'):
         list_of_synsets.append(synset.attrib['id'])
@@ -55,11 +109,16 @@ def test_for_existance_of_synset():
 
 ########## POS ###########
 
+def words_in_synset(id, wordnet):        
+    return(g_wordDict[id])
+    
+
 # 1. LexEntries mit mehreren Senses, die POS haben, das anders als das POS des Lemmas ist
 # 2. Alle Wörter mit Großbuchstaben, die POS v oder a haben
 # 3. Wörter auf -lich und -isch, die kein Adjektiv sind.
 
-def test_lexentries_pos():
+def test_lexentries_pos(wordnet):
+    lexicon = get_wordnet_lexicon_local(wordnet)
 #    out = open("out.txt","w",encoding="utf-8")
     for lexentry in lexicon.iter('LexicalEntry'):
         lemma = lexentry.find('Lemma')
@@ -72,35 +131,35 @@ def test_lexentries_pos():
         for sense in lexentry.iter('Sense'):
             synset_id = sense.attrib['synset']
             if not synset_id[-1] == pos:
-                words = words_in_synset(synset_id)
+                words = words_in_synset(synset_id,wordnet)
 #                out.write("-- " + lemma_value + " : " + pos + ": " + synset_id + " " + str(words) + " \n\n")
                 print("-- " + lemma_value + " : " + pos + ": " + synset_id + " " + str(words) + " \n\n")
         if re.match(r'^[A-ZÄÜÖ][a-zäöüß]+$',lemma_value):
             if pos == 'v':
                 synset_id = sense.attrib['synset']
-                words = words_in_synset(synset_id)
+                words = words_in_synset(synset_id,wordnet)
 #                out.write("-- " + lemma_value + " : " + pos + ": " + synset_id + " " + str(words) + " \n\n")
                 print("-- " + lemma_value + " : " + pos + ": " + synset_id + " " + str(words) + " \n\n")
         if re.match(r'^[a-zäöü]+lich$',lemma_value):
             if pos == 'n':
                 synset_id = sense.attrib['synset']
-                words = words_in_synset(synset_id)
+                words = words_in_synset(synset_id,wordnet)
 #                out.write("-- " + lemma_value + " : " + pos + ": " + synset_id + " " + str(words) + " \n\n")
                 print("-- " + lemma_value + " : " + pos + ": " + synset_id + " " + str(words) + " \n\n")
             elif pos == 'v':
                 synset_id = sense.attrib['synset']
-                words = words_in_synset(synset_id)
+                words = words_in_synset(synset_id,wordnet)
 #                out.write("-- " + lemma_value + " : " + pos + ": " + synset_id + " " + str(words) + " \n\n")
                 print("-- " + lemma_value + " : " + pos + ": " + synset_id + " " + str(words) + " \n\n")
         if re.match(r'^[a-zäöü]+isch$',lemma_value):
             if pos == 'n':
                 synset_id = sense.attrib['synset']
-                words = words_in_synset(synset_id)
+                words = words_in_synset(synset_id,wordnet)
 #                out.write("-- " + lemma_value + " : " + pos + ": " + synset_id + " " + str(words) + " \n\n")
                 print("-- " + lemma_value + " : " + pos + ": " + synset_id + " " + str(words) + " \n\n")
             elif pos == 'v':
                 synset_id = sense.attrib['synset']
-                words = words_in_synset(synset_id)
+                words = words_in_synset(synset_id,wordnet)
 #                out.write("-- " + lemma_value + " : " + pos + ": " + synset_id + " " + str(words) + " \n\n")
                 print("-- " + lemma_value + " : " + pos + ": " + synset_id + " " + str(words) + " \n\n")
 #    out.close()
@@ -110,18 +169,18 @@ def test_lexentries_pos():
 # Prüfung darauf, ob alle POS in einem Synset dieselben sind
 # 1. stimmt der POS aus der ID mit dem aus dem Attribut überein?
 
-def test_synsets_pos():
+def test_synsets_pos(wordnet):
+    lexicon = get_wordnet_lexicon_local(wordnet)
     for synset in lexicon.iter('Synset'):
         if synset.attrib['id'][-1] != synset.attrib['partOfSpeech']:
             print("Multiple POS values in " + synset.attrib['id'])
 
 
 # Prüfung darauf, ob POS in Synset und Relation Target übereinstimmen
-# Das sind immer noch ziemlich viele
-# Daher nicht in Standard-Test aufgenommen.
 
 
-def test_relation_pos():
+def test_relation_pos(wordnet):
+    lexicon = get_wordnet_lexicon_local(wordnet)
     for synset in lexicon.iter('Synset'):
         synset_pos = synset.attrib['partOfSpeech']
         for relation in synset.iter('SynsetRelation'):
@@ -135,9 +194,10 @@ def test_relation_pos():
 
 # Prüfung auf Synsets ohne LexEntries
 
-def test_for_empty_synsets():
+def test_for_empty_synsets(wordnet):
+    lexicon = get_wordnet_lexicon_local(wordnet)
     for synset in lexicon.iter('Synset'):
-        wlist = words_in_synset(synset.attrib['id'])
+        wlist = words_in_synset(synset.attrib['id'],wordnet)
         if len(wlist) == 0:
             print("Synset " + synset.attrib['id'] + " has no words!")
 
@@ -159,7 +219,8 @@ def is_valid_synset_id(xml_id):
 def is_valid_sense_id(xml_id):
     return bool(valid_sense_id.match(xml_id))
 
-def test_for_valid_ids():
+def test_for_valid_ids(wordnet):
+        lexicon = get_wordnet_lexicon_local(wordnet)
         for lexentry in lexicon.iter('LexicalEntry'):
             if not is_valid_word_id(lexentry.attrib['id']):
                 print(lexentry.attrib['id'] + " is not valid")
@@ -173,10 +234,9 @@ def test_for_valid_ids():
                     print("Synset ID " + synset.attrib['id'] + " is not valid")
                 
 # Prüfen, ob ilis doppelt vergeben wurden
-# Das sind auch ganz schön viele!
-# Nicht in Standard-Test aufgenommen.
 
-def test_for_duplicate_ilis():
+def test_for_duplicate_ilis(wordnet):
+    lexicon = get_wordnet_lexicon_local(wordnet)
     ili_list = []
     for synset in lexicon.iter('Synset'):
         synset_ili = synset.attrib['ili']
@@ -193,7 +253,8 @@ def test_for_duplicate_ilis():
 # 2. Prüfung auf Loops: Synsets, die als Target einer Relation sich selbst haben
 
 
-def test_target_in_relation():
+def test_target_in_relation(wordnet):
+    lexicon = get_wordnet_lexicon_local(wordnet)
     for synset in lexicon.iter('Synset'):
         id = synset.attrib['id']
         for relation in synset.iter('SynsetRelation'):
@@ -214,7 +275,8 @@ def test_target_in_relation():
 '''
 # 1. Ich erstelle eine Liste von allen Hypernym-Relationen und eine Liste von allen Hyponym-Relationen
 
-def get_all_hyponym_hypernym_relations():
+def get_all_hyponym_hypernym_relations(wordnet):
+    lexicon = get_wordnet_lexicon_local(wordnet)
     hyponym_relations = []
     hypernym_relations = []
     for synset in lexicon.iter('Synset'):
@@ -248,8 +310,8 @@ def test_for_missing_symmetry(hyponym_relations,hypernym_relations):
         if converse_relation not in hyponym_relations:
             print("Asymmetric relation: " + str(relation))
 
-def test_for_loops_in_relations():
-    hyponym_relations,hypernym_relations = get_all_hyponym_hypernym_relations()
+def test_for_loops_in_relations(wordnet):
+    hyponym_relations,hypernym_relations = get_all_hyponym_hypernym_relations(wordnet)
     test_for_loops_in_hypo_relations(hyponym_relations)
     test_for_loops_in_hypo_relations(hypernym_relations)
     test_for_missing_symmetry(hyponym_relations,hypernym_relations)
@@ -257,7 +319,8 @@ def test_for_loops_in_relations():
  
 # höchste Synset-ID
 
-def highest_synset_id():
+def highest_synset_id(wordnet):
+    lexicon = get_wordnet_lexicon_local(wordnet)
     top_id = 0
     for synset in lexicon.iter('Synset'):
         synset_id = synset.attrib['id']
@@ -271,7 +334,8 @@ def highest_synset_id():
 
 # höchste LexEntry-ID
 
-def highest_lex_id():
+def highest_lex_id(wordnet):
+    lexicon = get_wordnet_lexicon_local(wordnet)
     top_id = 0
     for lexentry in lexicon.iter('LexicalEntry'):
         lex_id = lexentry.attrib['id'][1:]
@@ -284,34 +348,42 @@ def highest_lex_id():
     
 # Test auf valides XML
 
-def test_valid_xml():
+def test_valid_xml(wordnet):
     parser = etree.XMLParser(dtd_validation=True, no_network=False)
-    tree = etree.parse("odenet/wordnet/deWordNet.xml", parser)
+    tree = etree.parse(wordnet, parser)
 
 
-def test_necessary_conditions():
+def test_necessary_conditions(wordnet):
+#    lexicon = get_wordnet_lexicon_local(wordnet)
+    g_wordDict = get_word_dict(wordnet)
     print("Test for valid xml ...")
-    test_valid_xml()
-    highest_synset_id()
-    highest_lex_id()
+    test_valid_xml(wordnet)
+    highest_synset_id(wordnet)
+    highest_lex_id(wordnet)
     print("Test for inconstent IDs in LexEntries ...")
-    find_inconsistent_lexids()
+    find_inconsistent_lexids(wordnet)
     print("Test for POS in LexEntries ...")
-    test_lexentries_pos()
+    test_lexentries_pos(wordnet)
     print("Test for POS in Synsets ...")
-    test_synsets_pos()
+    test_synsets_pos(wordnet)
+    print("Test for POS in Relations ...")
+    test_relation_pos(wordnet)
     print("Test for valid IDs")
-    test_for_valid_ids()
+    test_for_valid_ids(wordnet)
     print("Test for targets in relations ...")
-    test_target_in_relation()
+    test_target_in_relation(wordnet)
     print("Test for loops and missing symmetry in relations ...")
-    test_for_loops_in_relations()
+    test_for_loops_in_relations(wordnet)
     print("Test for existance of synsets ...")
-    test_for_existance_of_synset()
-#    print("Test for duplicate LexEntries ...") # sind ganz schön viele
-#    find_duplicate_lexentries()
-#    print("Test for synsets without words ...") 
-#    test_for_empty_synsets() # dauert ganz schön lange
+    test_for_existance_of_synset(wordnet)
+    print("Test for duplicate ilis...")
+    test_for_duplicate_ilis(wordnet)
+#    print("Test for duplicate LexEntries ...") 
+#    find_duplicate_lexentries(wordnet)
+    print("Test for synsets without words ...") 
+    test_for_empty_synsets(wordnet) 
 
-    
-    
+
+#Main Program
+g_wordDict = get_word_dict('odenet/wordnet/deWordNet.xml')
+test_necessary_conditions('odenet/wordnet/deWordNet.xml')
